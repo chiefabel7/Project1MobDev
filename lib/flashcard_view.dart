3e12ccabf13085f6b19fa9fb36fab31ab7468088
farsoomrohan <118229631+farsoomrohan@users.noreply.github.com>
2024-03-17 18:57:25 -0400

    Update flashcard_view.dart

--- lib/flashcard_view.dart
+++ lib/flashcard_view.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
 class FlashcardView extends StatelessWidget {
+  final String text;
   
-
   const FlashcardView({Key? key, required this.text}) : super(key: key);
 
   @override
-  
+  Widget build(BuildContext context) {
     return Card(
       elevation: 4,
-      
+      child: Center(child: Text(text, textAlign: TextAlign.center)),
     );
   }
 }
